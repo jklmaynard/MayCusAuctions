6c4ff292ae3dd1973e1e813f667f391333ec3f1c
James Maynard <jameskirkmaynard@gmail.com>
2015-03-05 12:12:48 -0800

    Add styling to images.

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ MayCusAuctions.Router.map(function() {
   this.resource('lots', {path: '/'});
   this.resource('lot', {path: '/:lot_id'}, function() {
     this.resource('new-item')
-    this.resource('item', {path: '/:item_id'});
   });
+  this.resource('item', {path: '/:item_id'});
   this.resource('new-lot');
   this.resource('history');
   this.resource('contact');
