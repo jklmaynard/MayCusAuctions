fb6dce9e30794fe277b4ff147c54c01c0a0a4e91
James Maynard <jameskirkmaynard@gmail.com>
2015-03-04 16:21:20 -0800

    Fix router bug.

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 MayCusAuctions.Router.map(function() {
   this.resource('lots', {path: '/'});
-  this.resource('new-lot');
   this.resource('lot', {path: ':lot_id'}, function() {
     this.resource('new-item');
+  this.resource('new-lot');
   });
   this.resource('history');
   this.resource('contact');
