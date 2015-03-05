4e7e4ba9038fd4726e1aa7c078db098de9c99897
James Maynard <jameskirkmaynard@gmail.com>
2015-03-05 11:06:05 -0800

    Straighten out paths route to individual item Objects so properties are displayed.

--- router.js
+++ router.js
@@ -1,7 +1,8 @@
 MayCusAuctions.Router.map(function() {
   this.resource('lots', {path: '/'});
   this.resource('lot', {path: '/:lot_id'}, function() {
-    this.resource('new-item');
+    this.resource('new-item')
+    this.resource('item', {path: '/:item_id'});
   });
   this.resource('new-lot');
   this.resource('history');
